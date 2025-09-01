f77af45970e0b3d7e85e6e405d145ebed560e12e
shaobolua <shaobolua@outlook.com>
2025-09-01 20:57:26 +0800

    变更包版本

--- internal/vite-config/build.config.ts
+++ internal/vite-config/build.config.ts
@@ -3,5 +3,5 @@ import { defineBuildConfig } from 'unbuild';
 export default defineBuildConfig({
   clean: true,
   declaration: true,
-  entries: ['src/index'],
+  entries: [{ builder: 'mkdist', input: './src' }],
 });
