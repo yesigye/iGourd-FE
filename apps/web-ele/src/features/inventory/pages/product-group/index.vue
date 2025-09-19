30304ac6ba01f1c9e56aba58c2270a357ced2c32
陈佳豪 <2805197210@qq.com>
2025-09-19 21:24:10 +0800

    feat(产品分组): 添加一级分类树并实现加载更多功能
    添加获取一级分类的API调用，将数据格式化为树形结构并展示。移除静态数据，改为动态加载分类树。当分类数量超过当前显示数量时，显示"加载更多"按钮。

--- apps/web-ele/src/features/inventory/pages/product-group/index.vue
+++ apps/web-ele/src/features/inventory/pages/product-group/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import type { Tree } from 'element-plus/es/components/tree-v2/src/types.mjs';
 
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 import { ColPage, ElButton, ElTree } from '@igourd/common-ui';
 import { useI18n } from '@igourd/locales';
 
+import { getFirstGroupList } from '../../apis/product-group';
 import { useInventoryProductGroupList } from '../../hooks/product-group/list';
 
 defineOptions({
@@ -63,24 +64,53 @@ const dataSource = ref<Tree[]>([
     ],
   },
 ]);
+const productGroupData = ref({
+  list: [],
+  page_num: 1,
+  page_size: 10,
+  total: 0,
+});
+// 获取一级分类
+const getFirstLevelCategory = async () => {
+  const result = await getFirstGroupList({ page_num: 1, page_size: 10 });
+  const list = result.list;
+  let treeList = [];
+  // 将list处理成element-plus的tree数据格式
+  treeList = list.map((item) => ({
+    id: item.id,
+    label: item.major_name,
+    children: [],
+  }));
+  productGroupData.value.list = treeList;
+  productGroupData.value.total = result.total;
+};
 const { Grid, Drawer, handleEdit, handleBatchDelete, canBatchOperate } =
   useInventoryProductGroupList();
+onMounted(async () => {
+  await getFirstLevelCategory();
+});
 </script>
 
 <template>
   <ColPage auto-content-height>
     <template #left="{ isCollapsed, expand }">
-      <section class="bg-card mb-5 h-full rounded p-2.5">
+      <section class="bg-card h-full rounded p-2.5">
         <p class="text-sm font-medium">
           {{ t('product-group.product_category') }}
         </p>
         <!-- 分类树 -->
-        <div>
-          <ElTree :data="dataSource" node-key="id">
+        <div class="mt-5">
+          <ElTree :data="productGroupData.list" node-key="id">
             <template #default="{ node, data }">
               <span>{{ node.label }}</span>
             </template>
           </ElTree>
+          <p
+            class="text-center"
+            v-if="productGroupData.total > productGroupData.list.length"
+          >
+            <ElButton type="primary" link>加载更多</ElButton>
+          </p>
         </div>
       </section>
     </template>
