27fdf493ad4efd5ec1185216cf019dbf5deac5d5
shaobolua <shaobolua@outlook.com>
2025-09-23 13:58:59 +0800

    修改跳转机制

--- packages/effects/access/src/use-account.ts
+++ packages/effects/access/src/use-account.ts
@@ -3,11 +3,9 @@ export function useAccount() {
     const jumpLoginUrl = location.pathname.includes('/401')
       ? location.origin
       : location.href;
-    window.open(
+    window.location.replace(
       `${import.meta.env.VITE_APP_API_LOGIN_URL}?redirect=${encodeURIComponent(jumpLoginUrl)}`,
-      '_self',
     );
-    // window.location.href = `${import.meta.env.VITE_APP_API_LOGIN_URL}?redirect=${encodeURIComponent(jumpLoginUrl)}`;
   }
 
   return {
