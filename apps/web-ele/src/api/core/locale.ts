60877acab9bf20e00f48f32d377869ed83a7aa89
shaobolua <shaobolua@outlook.com>
2025-09-30 11:37:19 +0800

    修复多语言

--- apps/web-ele/src/api/core/locale.ts
+++ apps/web-ele/src/api/core/locale.ts
@@ -1,12 +1,6 @@
 import { requestClient } from '#/api/request';
-import type { SupportedLanguagesType } from '@igourd/locales';
-type typeKey = 'zh_CN' | 'en' | 'fr';
 
-const supportLanguageMap: Record<typeKey, SupportedLanguagesType> = {
-  zh_CN: 'zh-CN',
-  en: 'en-US',
-  fr: 'fr-FR',
-};
+type typeKey = 'en' | 'fr' | 'zh_CN';
 
 export async function getLocaleApi(data: Record<string, any>) {
   return requestClient
@@ -14,14 +8,13 @@ export async function getLocaleApi(data: Record<string, any>) {
       Record<typeKey, { MERCHANT_MANAGE_WEB_PC: Record<string, string> }>
     >('/v1/passport/menu/language/list', data)
     .then((res) => {
+      // eslint-disable-next-line unicorn/no-array-reduce
       return Object.keys(res).reduce(
-        (acc, key) => {
-          const mappedKey = supportLanguageMap[key as typeKey];
-          acc[mappedKey as SupportedLanguagesType] =
-            res[key as typeKey].MERCHANT_MANAGE_WEB_PC;
+        (acc, key: string) => {
+          acc[key] = res[key as typeKey].MERCHANT_MANAGE_WEB_PC;
           return acc;
         },
-        {} as Record<SupportedLanguagesType, any>,
+        {} as Record<string, any>,
       );
     });
 }
