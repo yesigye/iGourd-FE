1d7dac7e0fc9cb33a8c4b4fc59d3eca84118d3f4
shaobolua <shaobolua@outlook.com>
2025-10-09 11:17:20 +0800

    调整表格样式

--- apps/web-ele/src/features/setting/pages/saleset/modal.vue
+++ apps/web-ele/src/features/setting/pages/saleset/modal.vue
@@ -32,7 +32,7 @@ function handleUpdate(len: number) {
     <div
       v-for="item in list"
       :key="item"
-      class="even:bg-heavy bg-muted flex-center h-[220px] w-full"
+      class="even:bg-heavy  flex-center h-[220px] w-full"
     >
       {{ item }}
     </div>
