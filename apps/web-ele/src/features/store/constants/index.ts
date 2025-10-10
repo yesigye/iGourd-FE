1c767e68cc6aee202b9b74c8b509ee97bee96ee4
shaobolua <shaobolua@outlook.com>
2025-10-10 10:31:22 +0800

    门店列表

---
+++ apps/web-ele/src/features/store/constants/index.ts
@@ -0,0 +1,6 @@
+export const BUSINESS_TYPE_CONFIG = [
+  { name: 'common.businessType_RETAIL', value: 'RETAIL' },
+  { name: 'common.businessType_WHOLESALE', value: 'WHOLESALE' },
+  { name: 'common.businessType_RESTAURANT', value: 'RESTAURANT' },
+  { name: 'common.businessType_COUNTING_GOODS', value: 'COUNTING_GOODS' },
+];
