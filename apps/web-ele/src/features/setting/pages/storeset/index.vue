d514046eb56d753b6d0c3b92b85c7e67533d647e
shaobolua <shaobolua@outlook.com>
2025-10-13 18:22:42 +0800

    修复构建报错

--- apps/web-ele/src/features/setting/pages/storeset/index.vue
+++ apps/web-ele/src/features/setting/pages/storeset/index.vue
@@ -162,7 +162,8 @@ onMounted(() => {
   <Page auto-content-height>
     <section class=" h-full text-xs">
       <p class="mb-4 flex items-center gap-2">
-      </p><div class="w-1 h-2.5 rounded-md bg-primary"></div> {{ t('storeset.basic-information') }}</p>
+      <div class="w-1 h-2.5 rounded-md bg-primary"></div> {{ t('storeset.basic-information') }}
+    </p>
       <!-- 设置项 -->
       <section class="pb-4 pl-5 pr-5 pt-4 bg-card">
         <!-- ----------设置------------ -->
