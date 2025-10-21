9d20480b21738b3233946a82a6e768d1c6749243
zhangfengman <zhangfengman@igourd.com>
2025-10-21 20:33:01 +0800

    feat(财会): 国际化

--- apps/web-ele/src/features/account/hooks/collection-voucher/form-schema.ts
+++ apps/web-ele/src/features/account/hooks/collection-voucher/form-schema.ts
@@ -45,11 +45,11 @@ export function useCollectionVoucherSchema() {
                 },
                 enum: [
                   {
-                    label: `{{t('collection-voucher.receipt_direction.positive_order')}}`,
+                    label: `{{t('collection-voucher.receipt-direction.positive-order')}}`,
                     value: 'POSITIVE_ORDER',
                   },
                   {
-                    label: `{{t('collection-voucher.receipt_direction.negative_order')}}`,
+                    label: `{{t('collection-voucher.receipt-direction.negative-order')}}`,
                     value: 'NEGATIVE_ORDER',
                   },
                 ],
