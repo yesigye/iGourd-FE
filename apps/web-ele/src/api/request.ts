dc8d5306dbf946b3dc37abeb2e2a0b1241e7e038
shaobolua <shaobolua@outlook.com>
2025-10-23 23:38:13 +0800

    前端微服务修复

--- apps/web-ele/src/api/request.ts
+++ apps/web-ele/src/api/request.ts
@@ -151,7 +151,7 @@ function createRequestClient(baseURL: string, options?: RequestClientOptions) {
 export const requestClient = createRequestClient(apiURL, {
   responseReturn: 'data',
 });
-export const arrayRequestClient = createRequestClient(apiURL, {
+export const arrayRequestClient = createRequestClient('/api/v1', {
   responseReturn: 'array',
 });
 export const baseRequestClient = new RequestClient({ baseURL: apiURL });
