a865f7410313aa6fed5ddfdec9917da082df9f34
zhangfengman <zhangfengman6235@163.com>
2025-11-03 18:50:57 +0800

    feat(调拨)：操作判断

--- apps/web-ele/src/features/inventory/pages/transfer/index.vue
+++ apps/web-ele/src/features/inventory/pages/transfer/index.vue
@@ -49,16 +49,10 @@ const auditDialogRef = ref();
 const openModal = (row, item) => {
   switch (row.transfer_type) {
     case 'TRANSFER_OUT_ONLY':
-      if (
-        row.review_status === 'PENDING' &&
-        item.value === 'REJECTED'
-      ) {
+      if (row.review_status === 'PENDING' && item.value === 'REJECTED') {
         currentRow.value = row;
         auditDialogRef.value.openModal();
-      } else if (
-        row.review_status === 'PENDING' &&
-        item.value === 'APPROVED'
-      ) {
+      } else if (row.review_status === 'PENDING' && item.value === 'APPROVED') {
         const param = {
           handler_type: 'SOURCE_REVIEW',
           id: row.id,
@@ -95,49 +89,102 @@ const openModal = (row, item) => {
       }
       break;
     case 'TRANSFER_SAME_STORE':
+      if (
+        row.destination_review_status === 'PENDING' &&
+        item.value === 'REJECTED'
+      ) {
+        currentRow.value = row;
+        auditDialogRef.value.openModal();
+      } else if (
+        row.destination_review_status === 'PENDING' &&
+        item.value === 'APPROVED'
+      ) {
+        const params = {
+          ...row,
+          ...item,
+        };
+        drawerApi.setData(params).open();
+      }
 
       break;
     case 'TRANSFER_DIFFERENT_STORE':
+      if (row.source_merchant_id === currentLoginUserApp.owner_id) {
+        if (row.review_status === 'PENDING' && item.value === 'REJECTED') {
+          currentRow.value = row;
+          auditDialogRef.value.openModal();
+        } else if (
+          row.review_status === 'PENDING' &&
+          item.value === 'APPROVED'
+        ) {
+          const param = {
+            handler_type: 'SOURCE_REVIEW',
+            id: row.id,
+            merchant_id: currentLoginUserApp.owner_id,
+            review_status: 'APPROVED',
+          };
+          reviewTransferStatus(param).then(() => {
+            auditDialogRef.value.closeModal();
+            gridApi.reload();
+          });
+        }
+      } else {
+        if (
+          row.destination_review_status === 'PENDING' &&
+          item.value === 'REJECTED'
+        ) {
+          currentRow.value = row;
+          auditDialogRef.value.openModal();
+        } else if (
+          row.destination_review_status === 'PENDING' &&
+          item.value === 'APPROVED'
+        ) {
+          const params = {
+            ...row,
+            ...item,
+          };
+          drawerApi.setData(params).open();
+        }
+      }
       break;
   }
 };
 
-const handleconfirm = (data:any) => {
+const handleconfirm = (data: any) => {
   const row = currentRow.value;
-  let params = null
+  let params = null;
   switch (row.transfer_type) {
     case 'TRANSFER_OUT_ONLY':
       params = {
-        review_status:"REJECTED",
-        id:row.id,
-        review_opinion:data.review_opinion,
-        handler_type:"SOURCE_REVIEW"
-      }
+        review_status: 'REJECTED',
+        id: row.id,
+        review_opinion: data.review_opinion,
+        handler_type: 'SOURCE_REVIEW',
+      };
       break;
     case 'TRANSFER_IN_ONLY':
       params = {
-        destination_review_status:"REJECTED",
-        id:row.id,
-        review_opinion:data.review_opinion,
-        handler_type:"DESTINATION_REVIEW"
-      }
+        destination_review_status: 'REJECTED',
+        id: row.id,
+        review_opinion: data.review_opinion,
+        handler_type: 'DESTINATION_REVIEW',
+      };
 
       break;
     case 'TRANSFER_SAME_STORE':
-       params = {
-        destination_review_status:"REJECTED",
-        id:row.id,
-        review_opinion:data.review_opinion,
-        handler_type:"DESTINATION_REVIEW"
-      }
+      params = {
+        destination_review_status: 'REJECTED',
+        id: row.id,
+        review_opinion: data.review_opinion,
+        handler_type: 'DESTINATION_REVIEW',
+      };
       break;
     case 'TRANSFER_DIFFERENT_STORE':
       params = {
-        destination_review_status:"REJECTED",
-        id:row.id,
-        review_opinion:data.review_opinion,
-        handler_type:"DESTINATION_REVIEW"
-      }
+        destination_review_status: 'REJECTED',
+        id: row.id,
+        review_opinion: data.review_opinion,
+        handler_type: 'DESTINATION_REVIEW',
+      };
       break;
   }
   reviewTransferStatus(params).then(() => {
@@ -151,35 +198,19 @@ const filterOpt = (keys) => {
   return list;
 };
 const getLabel = (row) => {
-  let currentStatus = '';
-  //调拨类型
-  switch (row.transfer_type) {
-    case 'TRANSFER_OUT_ONLY':
-      currentStatus = row.status;
-      break;
-    case 'TRANSFER_IN_ONLY':
-      currentStatus = row.destination_status;
-      break;
-    case 'TRANSFER_SAME_STORE':
-      currentStatus = row.status;
-      break;
-    case 'TRANSFER_DIFFERENT_STORE':
-      currentStatus = row.status;
-      break;
-  }
-  return getEnumLabel(transferStatus, currentStatus);
+  return getEnumLabel(transferStatus, row.overall_status);
 };
 
 //获取操作状态
 const getOperateStatus = computed(() => (row) => {
-  let status = "";
+  let status = '';
 
   switch (row.transfer_type) {
     case 'TRANSFER_OUT_ONLY':
       status = row['status'];
       break;
     case 'TRANSFER_IN_ONLY':
-       status = row['destination_status'];
+      status = row['destination_status'];
       break;
     case 'TRANSFER_SAME_STORE':
       status = row['destination_status'];
@@ -193,91 +224,44 @@ const getOperateStatus = computed(() => (row) => {
 });
 //获取状态选项
 const getStatusOptions = computed(() => (row) => {
-  const baseOptions = [{ value: 'CREATED', label: 'Created', key: 'created' }];
-
-  //调拨类型
+  // 创建成功	调拨出库	审核通过	审核拒绝	调拨入库	审核通过	审核拒绝
+  // CREATED(操作：调拨出库)	OUTBOUND(操作：审核)	APPROVED_OUTBOUND(操作：调拨入库)	REJECTED_OUTBOUND	INBOUND(操作：审核)	APPROVED_INBOUND	REFUSED_INBOUND
   switch (row.transfer_type) {
     case 'TRANSFER_OUT_ONLY':
-      return filterOpt(['OUTBOUND']);
+      if (row.overall_status === 'CREATED') {
+        return filterOpt(['OUTBOUND']);
+      }
+
     case 'TRANSFER_IN_ONLY':
-      // 入库 看 destination_status 状态
-      if (row.destination_status == 'CREATED') {
-        return filterOpt(['CREATED', 'INBOUND']);
-      } else {
+      if (row.overall_status === 'CREATED') {
         return filterOpt(['INBOUND']);
       }
       break;
-
     case 'TRANSFER_SAME_STORE':
-      if (row.status == 'CREATED') {
+      if (row.overall_status === 'CREATED') {
         return filterOpt(['OUTBOUND']);
-      } else if (row.status == 'OUTBOUND') {
+      } else if (row.overall_status === 'OUTBOUND') {
         return filterOpt(['INBOUND']);
       }
       break;
     case 'TRANSFER_DIFFERENT_STORE':
       if (row.source_merchant_id === currentLoginUserApp.owner_id) {
-        // 源门店（出库）选项
-        if (row.status != 'CREATED') {
-          return [
-            { value: 'OUTBOUND', label: 'Transfer Out', key: 'transfer_out' },
-            {
-              value: 'REFUSED_OUTBOUND',
-              label: 'Reject Out',
-              key: 'reject_out',
-            },
-          ];
+        if (row.overall_status === 'CREATED') {
+          return filterOpt(['OUTBOUND']);
+        } else if (row.overall_status === 'OUTBOUND') {
+          return [];
         }
-        return [
-          ...baseOptions,
-          { value: 'OUTBOUND', label: 'Transfer Out', key: 'transfer_out' },
-          // { value: 'REFUSED_OUTBOUND', label: 'Reject Out', key: 'reject_out' }
-        ];
-      } else if (
-        row.destination_merchant_id === currentLoginUserApp.owner_id &&
-        row.status === 'OUTBOUND' &&
-        row.review_status === 'APPROVED'
-      ) {
-        // 目标门店（入库）选项
-        if (row.status != 'CREATED') {
-          return [
-            { value: 'INBOUND', label: 'Transfer In', key: 'transfer_in' },
-            // { value: 'REFUSED_INBOUND', label: 'Reject In', key: 'reject_in' }
-            { value: 'OUTBOUND', label: 'Transfer Out', key: 'transfer_out' },
-          ];
-        }
-        return [
-          ...baseOptions,
-          { value: 'INBOUND', label: 'Transfer In', key: 'transfer_in' },
-          // { value: 'REFUSED_INBOUND', label: 'Reject In', key: 'reject_in' }
-        ];
       } else {
-        if (row.status != 'CREATED') {
-          return [
-            { value: 'INBOUND', label: 'Transfer In', key: 'transfer_in' },
-            { value: 'OUTBOUND', label: 'Transfer Out', key: 'transfer_out' },
-            // { value: 'REFUSED_INBOUND', label: 'Reject In', key: 'reject_in' }
-            {
-              value: 'REFUSED_OUTBOUND',
-              label: 'Reject Out',
-              key: 'reject_out',
-            },
-          ];
+        if (row.overall_status === 'CREATED') {
+          return [];
         }
-        return [
-          ...baseOptions,
-          { value: 'INBOUND', label: 'Transfer In', key: 'transfer_in' },
-          // { value: 'REFUSED_INBOUND', label: 'Reject In', key: 'reject_in' },
-          { value: 'OUTBOUND', label: 'Transfer Out', key: 'transfer_out' },
-          // { value: 'REFUSED_OUTBOUND', label: 'Reject Out', key: 'reject_out' }
-        ];
       }
-
+      break;
     default:
-      return baseOptions;
+      return [];
   }
 
-  return transferStatus;
+  return [];
 });
 const handleStatusChange = async (row, value) => {
   const params = {
@@ -313,12 +297,15 @@ const checkOperate = (row) => {
   //调拨类型
   switch (row.transfer_type) {
     case 'TRANSFER_OUT_ONLY':
-      if (row.status === 'OUTBOUND' ||  row.status==="REFUSED_OUTBOUND") {
+      if (row.status === 'OUTBOUND' || row.status === 'REFUSED_OUTBOUND') {
         flag = false;
       }
       break;
     case 'TRANSFER_IN_ONLY':
-      if (row.destination_status === 'INBOUND' || row.destination_status==="REFUSED_INBOUND") {
+      if (
+        row.destination_status === 'INBOUND' ||
+        row.destination_status === 'REFUSED_INBOUND'
+      ) {
         flag = false;
       }
       break;
@@ -338,36 +325,59 @@ const checkOperate = (row) => {
   return flag;
 };
 // 判断审核操作
-const checkReviewOperate = (row:any) => {
-  if(row.id=="1984250235206053890"){
-    debugger
+const checkReviewOperate = (row: any) => {
+  if (row.id == '1985243151210721281') {
   }
   let flag = false;
   //调拨类型
   switch (row.transfer_type) {
     case 'TRANSFER_OUT_ONLY':
-      if (row.status === 'OUTBOUND') {
-         flag = true;
+      if (row.overall_status === 'OUTBOUND') {
+        flag = true;
       }
       break;
     case 'TRANSFER_IN_ONLY':
-      if (row.destination_status === 'INBOUND' && row.destination_review_status ==='PENDING') {
+      if (row.overall_status === 'INBOUND') {
         flag = true;
       }
       break;
     case 'TRANSFER_SAME_STORE':
-      if (row.destination_status === 'INBOUND') {
+      if (row.overall_status === 'INBOUND') {
         flag = true;
       }
 
       break;
     case 'TRANSFER_DIFFERENT_STORE':
-      if (row.destination_status === 'INBOUND') {
-        flag = true;
+      if (row.source_merchant_id === currentLoginUserApp.owner_id) {
+        if (row.overall_status === 'OUTBOUND') {
+          flag = true;
+        }
+      } else {
+        if (row.overall_status === 'INBOUND') {
+          flag = true;
+        }
       }
+
       break;
   }
   return flag;
+};
+// 获取审核文字
+const getReviewLabel = (row: any) => {
+  console.log('getReviewLabel');
+  if (
+    row.overall_status == 'APPROVED_OUTBOUND' ||
+    row.overall_status == 'APPROVED_INBOUND'
+  ) {
+    return getEnumLabel(operationOpt.value, 'APPROVED');
+  } else if (
+    row.overall_status == 'REJECTED_OUTBOUND' ||
+    row.overall_status == 'REFUSED_INBOUND'
+  ) {
+    return getEnumLabel(operationOpt.value, 'REJECTED');
+  } else {
+    return getEnumLabel(operationOpt.value, 'PENDING');
+  }
 };
 </script>
 
@@ -396,7 +406,7 @@ const checkReviewOperate = (row:any) => {
         </ElButton>
       </template>
       <template #status="{ row }">
-        <ElDropdown v-if="checkOperate(row)">
+        <ElDropdown v-if="getStatusOptions(row).length > 0">
           <span class="custom-dropdown">
             {{ getLabel(row) }}
             <ElIcon class="el-icon--right">
@@ -416,13 +426,12 @@ const checkReviewOperate = (row:any) => {
             </ElDropdownMenu>
           </template>
         </ElDropdown>
-        <span v-if="getOperateStatus(row) ==='OUTBOUND' || getOperateStatus(row) === 'INBOUND'"  class="review-approved"> {{ getLabel(row) }}</span>
-        <span v-if="getOperateStatus(row) ==='REFUSED_OUTBOUND' || getOperateStatus(row) ==='REFUSED_INBOUND'" class="review-rejected"> {{ getLabel(row) }}</span>
+        <span v-else class="review-approved"> {{ getLabel(row) }}</span>
       </template>
       <template #review_status="{ row }">
         <ElDropdown v-if="checkReviewOperate(row)">
           <span class="custom-dropdown">
-            {{ getEnumLabel(operationOpt, getReviewStatus(row)) }}
+            {{ getReviewLabel(row) }}
             <ElIcon class="el-icon--right">
               <ArrayDown />
             </ElIcon>
@@ -437,9 +446,7 @@ const checkReviewOperate = (row:any) => {
             </ElDropdownMenu>
           </template>
         </ElDropdown>
-        <span v-if="getReviewStatus(row) ==='APPROVED'" class="review-approved" >{{ getEnumLabel(operationOpt, getReviewStatus(row)) }}</span>
-        <span v-if="getReviewStatus(row) ==='REJECTED'"  class="review-rejected" >{{ getEnumLabel(operationOpt, getReviewStatus(row)) }}</span>
-         <span v-if="getReviewStatus(row) ==='PENDING' && !checkReviewOperate(row)"   >{{ getEnumLabel(operationOpt, getReviewStatus(row)) }}</span>
+        <span v-else> {{ getReviewLabel(row) }}</span>
       </template>
     </Grid>
 
